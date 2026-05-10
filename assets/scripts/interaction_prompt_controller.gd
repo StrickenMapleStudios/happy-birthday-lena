06f1ae9e35278e2b343ad4f3e0ccdf2da75b4c8f
StrickenMaple Studios <strickenmaple05@mail.ru>
2026-05-10 18:23:13 +0300

    Some changes

--- assets/scripts/interaction_prompt_controller.gd
+++ assets/scripts/interaction_prompt_controller.gd
@@ -5,18 +5,23 @@ class_name InteractionPromptController
 const PROMPT_SCENE := preload("res://assets/scenes/common/interaction_prompt_3d.tscn")
 
 @export var gameplay_ui_layer_path: NodePath = ^"../GameplayUI"
+@export var player_reference_path: NodePath = ^"../character"
 @export_range(0.1, 10.0, 0.1) var near_distance := 3.0
 @export_range(0.1, 50.0, 0.1) var far_distance := 14.0
 @export_range(0.1, 4.0, 0.01) var max_prompt_scale := 1.0
 @export_range(0.1, 4.0, 0.01) var min_prompt_scale := 0.65
+@export_range(0.0, 200.0, 1.0) var screen_side_offset := 26.0
+@export_range(-200.0, 200.0, 1.0) var screen_vertical_offset := -8.0
 
 var _current_target: InteractionTarget
 var _prompt: InteractionPrompt3D
 var _gameplay_ui_layer: GameplayUiLayer
+var _player_reference: Node3D
 
 
 func _ready() -> void:
 	_gameplay_ui_layer = get_node_or_null(gameplay_ui_layer_path) as GameplayUiLayer
+	_player_reference = get_node_or_null(player_reference_path) as Node3D
 	_prompt = PROMPT_SCENE.instantiate() as InteractionPrompt3D
 	if _gameplay_ui_layer != null:
 		_gameplay_ui_layer.add_world_ui(_prompt)
@@ -36,17 +41,13 @@ func _process(_delta: float) -> void:
 		_prompt.hide_prompt()
 		return
 
-	var anchor := _current_target.get_interaction_prompt_anchor()
-	if anchor == null:
-		anchor = _current_target
-
-	var anchor_position := anchor.global_position
-	if camera.is_position_behind(anchor_position):
+	var prompt_position := _get_prompt_world_position()
+	if camera.is_position_behind(prompt_position):
 		_prompt.hide_prompt()
 		return
 
-	_prompt.set_visual_scale(_get_prompt_scale(camera.global_position.distance_to(anchor_position)))
-	_prompt.set_screen_position(camera.unproject_position(anchor_position))
+	_prompt.set_visual_scale(_get_prompt_scale(camera.global_position.distance_to(prompt_position)))
+	_prompt.set_screen_position(_get_prompt_screen_position(camera, prompt_position))
 	_prompt.show_prompt()
 
 
@@ -60,15 +61,16 @@ func set_target(target: InteractionTarget) -> void:
 		return
 
 	_prompt.key_text = _current_target.get_interaction_key_text()
-	var anchor := _current_target.get_interaction_prompt_anchor()
-	if anchor == null:
-		anchor = _current_target
 	var camera := get_viewport().get_camera_3d()
-	if camera == null or camera.is_position_behind(anchor.global_position):
+	if camera == null:
 		_prompt.hide_prompt()
 		return
-	_prompt.set_visual_scale(_get_prompt_scale(camera.global_position.distance_to(anchor.global_position)))
-	_prompt.set_screen_position(camera.unproject_position(anchor.global_position))
+	var prompt_position := _get_prompt_world_position()
+	if camera.is_position_behind(prompt_position):
+		_prompt.hide_prompt()
+		return
+	_prompt.set_visual_scale(_get_prompt_scale(camera.global_position.distance_to(prompt_position)))
+	_prompt.set_screen_position(_get_prompt_screen_position(camera, prompt_position))
 	_prompt.show_prompt()
 
 
@@ -78,3 +80,23 @@ func _get_prompt_scale(distance_to_camera: float) -> float:
 
 	var weight := clampf(inverse_lerp(near_distance, far_distance, distance_to_camera), 0.0, 1.0)
 	return lerpf(max_prompt_scale, min_prompt_scale, weight)
+
+
+func _get_prompt_world_position() -> Vector3:
+	if _current_target == null:
+		return Vector3.ZERO
+
+	var player_position := _current_target.global_position
+	if _player_reference != null:
+		player_position = _player_reference.global_position
+
+	return _current_target.get_interaction_prompt_position(player_position)
+
+
+func _get_prompt_screen_position(camera: Camera3D, prompt_position: Vector3) -> Vector2:
+	var world_screen_position := camera.unproject_position(prompt_position)
+	if _current_target == null:
+		return world_screen_position
+
+	var side_sign := _current_target.get_interaction_prompt_side_sign(camera.global_position)
+	return world_screen_position + Vector2(side_sign * screen_side_offset, screen_vertical_offset)
