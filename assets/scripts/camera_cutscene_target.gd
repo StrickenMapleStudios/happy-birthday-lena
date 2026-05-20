8d0ddb0814bdc513f579df19214d47f31473002f
n0th1ngn355 <strickenmaple05@mail.ru>
2026-05-20 21:21:21 +0300

    Добавлены пивоты для персонажа и камеры в mystery room

--- assets/scripts/camera_cutscene_target.gd
+++ assets/scripts/camera_cutscene_target.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export var cutscene_camera_path: NodePath = ^"CutsceneCamera"
 @export var player_anchor_path: NodePath = ^"PlayerCutsceneAnchor"
+@export var dialogue_camera_path: NodePath = ^"DialogueCamera"
+@export var dialogue_player_anchor_path: NodePath = ^"PlayerDialogueAnchor"
 @export var return_player_to_origin_after_cutscene := false
 @export var preserve_player_height_during_cutscene := true
 @export var dialogue_speaker_name := "Cake"
@@ -24,15 +26,31 @@ func should_preserve_player_height_during_cutscene() -> bool:
 
 
 func get_dialogue_camera_mount() -> Node3D:
+	var dialogue_camera := get_node_or_null(dialogue_camera_path) as Camera3D
+	if dialogue_camera != null:
+		return dialogue_camera
+
 	return get_cutscene_camera()
 
 
 func get_dialogue_scene_camera() -> Camera3D:
+	var dialogue_camera := get_node_or_null(dialogue_camera_path) as Camera3D
+	if dialogue_camera != null:
+		return dialogue_camera
+
 	return get_cutscene_camera()
 
 
+func get_player_dialogue_anchor() -> Node3D:
+	var dialogue_player_anchor := get_node_or_null(dialogue_player_anchor_path) as Node3D
+	if dialogue_player_anchor != null:
+		return dialogue_player_anchor
+
+	return get_player_cutscene_anchor()
+
+
 func get_dialogue_focus_position() -> Vector3:
-	var player_anchor := get_player_cutscene_anchor()
+	var player_anchor := get_player_dialogue_anchor()
 	if player_anchor != null:
 		return player_anchor.global_position
 
